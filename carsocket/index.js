2fd47a770437450c0b1b705dbe27c25e5f930269
Jorge <jorge2034@gmail.com>
2023-02-19 04:54:16 -0400

    correcion url socket

--- carsocket/index.js
+++ carsocket/index.js
@@ -49,6 +49,6 @@ io.on('connection', (socket) => {
   }
   
 
-http.listen(3002, () => {
-  console.log('listening on *:3002');
+http.listen(3000, () => {
+  console.log('listening on *:3000');
 });
